b77e64b1fe80cf85fa54fd6711a62417f0045d5a
akgyapong <kgamoako002@st.ug.edu.gh>
2025-11-06 11:43:41 +0000

    Complete login and sign up api

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
-
+from .views import PasswordResetConfirmView, PasswordResetRequestView
 urlpatterns = [
     path('health/', views.health_check, name='health_check'),
     path('auth/register/', views.user_register, name='user_register'),
     path('auth/login/', views.login_user, name='login_user'),
+    path('password-reset/request/', PasswordResetRequestView.as_view(), name='password-reset-request'),
+    path('password-reset/confirm/', PasswordResetConfirmView.as_view(), name='password-reset-confirm'),
 ]
